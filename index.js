23973578c9164d166608a55a0b372f583fdc1fab
Gleb Makagonov <bywildeme@gmail.com>
2017-05-08 13:48:26 +0500

    return statement in message.send, message.inline, message.lines

--- index.js
+++ index.js
@@ -119,14 +119,14 @@ const Telega = function(arg) {
 		message.search = parsed.search || "";
 
 		message.send = function() {
-			_this.api.sendMessage({
+			return _this.api.sendMessage({
 				chat_id: message.chat.id,
 				text: [].join.call(arguments, " ")
 			});
 		}
 
 		message.lines = function(lines) {
-			message.send(typeof lines === "array" ? lines.join("\n") : [].join.call(arguments, "\n"));
+			return message.send(typeof lines === "array" ? lines.join("\n") : [].join.call(arguments, "\n"));
 		}
 
 		message._inline = function(text, data, many, handler) {
@@ -153,7 +153,7 @@ const Telega = function(arg) {
 				many: many
 			}
 
-			_this.api.sendMessage({
+			return _this.api.sendMessage({
 				chat_id: message.chat.id,
 				text: text,
 				reply_markup: JSON.stringify({ inline_keyboard: raws })
@@ -162,10 +162,10 @@ const Telega = function(arg) {
 
 		message.inline = overload()
 			.args(String, Array, Function).use(function(text, data, handler) {
-				message._inline(text, data, null, handler);
+				return message._inline(text, data, null, handler);
 			})
 			.args(String, Array, Boolean, Function).use(function(text, data, many, handler) {
-				message._inline(text, data, many, handler);
+				return message._inline(text, data, many, handler);
 			});
 
 		message.answer = function(handler) {
